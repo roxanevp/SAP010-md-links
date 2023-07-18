02b16ceff19aaeb46c9ba3ee0d9fe46c9e969527
Roxane Principe <roxane.principe@gmail.com>
2023-07-18 15:38:32 -0300

    teste de leitura de arquivo

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+#!/usr/bin/env node
+
 module.exports = () => {
-  // ...
+  console.log('bbbb')
 };
+
+if (require.main === module ) {
+
+}
+console.log('aaaa')
+
+const fs = require('fs')
+fs.readFile('./README.md' , 'utf8' ,  (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(data)
+})
