76d5bce860720598eb5dc216397b425fcf4b9523
Roxane Principe <roxane.principe@gmail.com>
2023-07-28 14:31:23 -0300

    feito função que valida se o link está saudavél

--- cli.js
+++ cli.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 const main = require('./index');
-//
-main('./exemplos').then((values) => console.log(JSON.stringify(values, null, 2)));
+
+// main('./exemplos').then((values) => console.log(JSON.stringify(values, null, 2)));
+console.log(typeof main.validateLink, main.validateLink);
+main.validateLink('https://twitter.com/exit/dimitri').then(console.log);
